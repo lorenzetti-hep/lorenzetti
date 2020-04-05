e71a0c75b066c46edbe2bb2f8fec17f3ceb04f3f
jodafons <jodafons@lps.ufrj.br>
2020-04-05 02:35:53 -0300

    add new job shape

--- scripts/run_cli.py
+++ scripts/run_cli.py
@@ -1,8 +1,50 @@
 
 from G4Kernel import ComponentAccumulator
 
+
+
+
+
+
 acc = ComponentAccumulator("ComponentAccumulator")
 acc.VisMacro = 'init_vis.mac' 
+
+
+
+ecal_tools = [
+                PulseGeneratorTool("PulseGenerator_ECal_Barrel", nSamples = 7, path = '/path/to/card.dat' ),
+                EnergyEstimatorTool( "EnergyEstimator_ECal_Barrel", path = '/path/to/card.dat' ),
+              ]
+          
+
+hcal_tools = [
+                PulseGeneratorTool("PulseGenerator_HCal_Barrel", nSamples = 7, path = '/path/to/card.dat' ),
+                EnergyEstimatorTool( "EnergyEstimator_HCal_Barrel", path = '/path/to/card.dat' ),
+              ]
+ 
+
+
+ecal_barrel = CaloCellMaker( "CaloCellMaker_ECal_Barrel", Tools = ecal_tools )
+hcal_barrel = CaloCellMaker( "CaloCellMaker_HCal_Barrel", Tools = hcal_tools )
+
+
+cluster_maker = CaloClusterMaker()
+
+rings = CaloRingsBuilder()
+
+output = PhysValOutput()
+
+
+# Make the sequence
+acc += ecal_barrel
+acc += hcal_barrel
+#acc += ecal_extbarrel
+#acc += hcal_extbarrel
+acc += cluster_maker
+acc += rings
+acc += output
+
+
 acc.run()
 
 
