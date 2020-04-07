a465c42bda383981b45988ef8cd274ca46636ca1
jodafons <jodafons@lps.ufrj.br>
2020-04-07 13:25:18 -0300

    add cli

--- scripts/run_cli.py
+++ scripts/run_cli.py
@@ -1,52 +1,32 @@
 
 from G4Kernel import ComponentAccumulator
+#from CaloRec import CaloClusterMaker, CaloCellMaker, PulseGenerator, OptimalFilter
+import os
+import ROOT
+ROOT.gSystem.Load('liblorenzetti')
+
+basepath = os.environ['LZT_PATH']+'/reconstruction/CaloRec/data'
 
 
 
+acc = ComponentAccumulator("ComponentAccumulator", VisMacro = "init_vis.mac", NumberOfThreads = 4)
+
+
+#from CaloRec import CaloCellBuilder
+#cells = CaloCellBuilder("CaloCellBuilder")
+#cells.merge(acc)
+
+#CollectionKeys = cells.keys() 
+
+#cluster = CaloClusterMaker( "CaloClusterMaker",
+#                            CollectionKeys = CollectionKeys
+#                            )
+#acc+= cluster
 
 
 
-acc = ComponentAccumulator("ComponentAccumulator")
-acc.VisMacro = 'init_vis.mac' 
-
-
-
-ecal_tools = [
-                PulseGeneratorTool("PulseGenerator_ECal_Barrel", nSamples = 7, path = '/path/to/card.dat' ),
-                EnergyEstimatorTool( "EnergyEstimator_ECal_Barrel", path = '/path/to/card.dat' ),
-              ]
-          
-
-hcal_tools = [
-                PulseGeneratorTool("PulseGenerator_HCal_Barrel", nSamples = 7, path = '/path/to/card.dat' ),
-                EnergyEstimatorTool( "EnergyEstimator_HCal_Barrel", path = '/path/to/card.dat' ),
-              ]
- 
-
-
-ecal_barrel = CaloCellMaker( "CaloCellMaker_ECal_Barrel", Tools = ecal_tools )
-hcal_barrel = CaloCellMaker( "CaloCellMaker_HCal_Barrel", Tools = hcal_tools )
-
-
-cluster_maker = CaloClusterMaker()
-
-rings = CaloRingsBuilder()
-
-output = PhysValOutput()
-
-
-# Make the sequence
-acc += ecal_barrel
-acc += hcal_barrel
-#acc += ecal_extbarrel
-#acc += hcal_extbarrel
-acc += cluster_maker
-acc += rings
-acc += output
-
-
+#acc.run('run_gun.mac')
 acc.run()
-
 
 
 
