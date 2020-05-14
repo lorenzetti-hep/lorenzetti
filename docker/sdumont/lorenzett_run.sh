ff9cdf81339dd7e27dec58cb9aaa708ea789ecc8
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2020-05-14 15:59:52 -0300

    Add optional recoscript and initial travis file

--- docker/sdumont/lorenzett_run.sh
+++ docker/sdumont/lorenzett_run.sh
@@ -6,7 +6,7 @@ print_usage () {
     echo " "
     echo "./lorenzett_run.sh [arguments]"
     echo " "
-    echo "Arguments:"
+    echo "Required arguments:"
     echo "-h, --help                show this message"
     echo "-f, --filter              chooses which type of event you're interested"
     echo "-e, --event               sets the desired number of events"
@@ -15,7 +15,9 @@ print_usage () {
     echo "--bc_id_start             sets bc_id_start"
     echo "--bc_id_end               sets bc_id_end"
     echo " "
-    echo "All arguments are required"
+    echo "Optional arguments:"
+    echo "-r, --reco                chooses recovery script"
+    echo " "
     exit 0
 }
 
@@ -36,6 +38,8 @@ fi
 while [[ $# -gt 0 ]]
 do
 key="$1"
+
+RECO_SCRIPT="default"
 
 case $key in
     -h|--help)
@@ -68,6 +72,11 @@ case $key in
     ;;
     --bc_id_end)
     BC_END="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -r|--reco)
+    RECO_SCRIPT="$2"
     shift # past argument
     shift # past value
     ;;
@@ -125,8 +134,22 @@ case ${FILTER} in
     ;;
 esac
 
+case ${RECO_SCRIPT} in
+    "default")
+    RECO_SCRIPT="reco_trf.py"
+    ;;
+    "digit")
+    RECO_SCRIPT="reco_trf.py"
+    ;;
+    *)
+    echo "Unknown option for --event, please choose one of the following: Zee, JF17"
+    exit 0
+    ;;
+esac
+
+
 # Run commands
 CPU_N=$(grep -c ^processor /proc/cpuinfo)
 source setup_envs.sh
 prun_job.py -c "generator.py --filter $FILTER -i $CONFIG --outputLevel 6 --seed 0 --evt ${EVENT} --pileupAvg ${PILEUP} --bc_id_start ${BC_START} --bc_id_end ${BC_END}" -mt $CPU_N -n 10 -o /output/generator_${OUTPUT}
-reco_trf.py -i /output/generator_${OUTPUT} --outputLevel 6 -nt $CPU_N -o /output/reco_${OUTPUT}
+$RECO_SCRIPT -i /output/generator_${OUTPUT} --outputLevel 6 -nt $CPU_N -o /output/reco_${OUTPUT}
