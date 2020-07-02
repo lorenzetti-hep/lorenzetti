8156583faabd3eb1e0488b87b3bbd69c14d3c2c4
jodafons <jodafons@lps.ufrj.br>
2020-07-02 23:47:13 +0000

    add new docker

--- docker/base/setup_envs.sh
+++ docker/base/setup_envs.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
+current_path=$PWD
 
 # Envs
 CPU_N=$(grep -c ^processor /proc/cpuinfo)
 
-# HEP local packages
-
 # ROOT
+echo "setup root..."
 source /opt/root/buildthis/bin/thisroot.sh
 
 # geant4
+echo "setup geant..."
 cd /opt/geant4/buildthis
-ls
-source geant4.sh
-cd /
+source /opt/geant4/buildthis/geant4.sh
 
 # pythia
+echo "setup pythia..."
 export PYTHIA8_INCLUDE=/opt/pythia/include
 export PYTHIA8_LIBRARIES=/opt/pythia/lib
 
 #fastjet
+echo "setup fastjet..."
 export FASTJET_INCLUDE=/opt/fastjet-3.3.3/include
 export FASTJET_LIBRARIES=/usr/local/lib
 
 #hepmc
+echo "setup hepmc..."
 export HEPMC_LIBRARIES=/usr/local/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HEPMC_LIBRARIES
 
+
+# preload libs (fix)
 export LD_PRELOAD=''
 for file in /opt/geant4/buildthis/BuildProducts/lib/*.so
 do
@@ -34,9 +38,12 @@ do
 done
 
 
-cd /code/lorenzetti
-git pull
-mkdir build && cd build && cmake .. && make -j$CPU_N && cd .. && source setup.sh
-
-
+echo "compile lorenzetti"
+git clone https://github.com/jodafons/lorenzetti.git
+cd lorenzetti
+source $PWD/setup_module.sh
+source $PWD/setup_module.sh --head
+mkdir build && cd build && cmake .. && make -j$CPU_N && cd .. && source $PWD/setup.sh
+echo "back to the base dir..."
+cd $current_path
 
--- docker/lps/run.sh
+++
@@ -1,3 +0,0 @@
-docker run --network host -v $PWD:/output -it lorenzetti/lorenzetti:lps /bin/bash
-
-
