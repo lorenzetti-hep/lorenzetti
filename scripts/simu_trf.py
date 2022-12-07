75e80ed417d7db7480140d0f81afb541dff97158
jlieberm <48608404+jlieberm@users.noreply.github.com>
2022-12-07 05:52:31 -0300

    Update simu_trf.py

--- scripts/simu_trf.py
+++ scripts/simu_trf.py
@@ -49,7 +49,7 @@ parser.add_argument('--saveAllHits', action='store_true', dest='saveAllHits', re
 parser.add_argument('-t','--timeout', action='store', dest='timeout', required = False, type=int, default=5,
                     help = "Event timeout in minutes")
 
-parser.add_argument('--isFromHepMC', action='store_true', dest='isFromHepMC', required = False,
+parser.add_argument('--isFromHepMC', action='store_true', dest='isFromHepMC', required = False, default = False,
                     help = "In debug mode.")
 
 pi = np.pi
@@ -104,7 +104,7 @@ try:
                      TruthKey   = recordable("Particles"),
                      FileName   = args.inputFile,
                      BunchDuration = 25.0,#ns
-                     isFromHepMC = True,
+                     isFromHepMC = args.isFromHepMC,
                      )
 
 
