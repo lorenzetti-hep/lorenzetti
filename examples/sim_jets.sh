a8dae8025c8382615ac3a431704e2a3dd8f97ea5
jodafons <jodafons@lps.ufrj.br>
2022-12-29 21:37:30 -0500

    update

--- examples/sim_jets.sh
+++ examples/sim_jets.sh
@@ -23,7 +23,7 @@ prun_jobs.py -c "digit_trf.py -i %IN -o %OUT" -i ../HIT/ -o JF17.ESD.root -nt 40
 cd ..
 
 # reconstruction
-cd ../AOD
-prun_jobs.py -c "reco_trf.py -i %IN -o %OUT" -i ../ESD/ -o JF17.AOD.root -mt 40 -m
+cd AOD
+prun_jobs.py -c "reco_trf.py -i %IN -o %OUT" -i ../ESD/ -o JF17.AOD.root -nt 40 -m
 cd ..
 
--- examples/sim_zee.sh
+++ examples/sim_zee.sh
@@ -23,7 +23,7 @@ prun_jobs.py -c "digit_trf.py -i %IN -o %OUT" -i ../HIT/ -o Zee.ESD.root -nt 40
 cd ..
 
 # reconstruction
-cd ../AOD
-prun_jobs.py -c "reco_trf.py -i %IN -o %OUT" -i ../ESD/ -o Zee.AOD.root -mt 40 -m
+cd AOD
+prun_jobs.py -c "reco_trf.py -i %IN -o %OUT" -i ../ESD/ -o Zee.AOD.root -nt 40 -m
 cd ..
 
