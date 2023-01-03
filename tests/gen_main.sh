15e690561ef2d2ca3a8bf93038cb183da710931b
jodafons <jodafons@lps.ufrj.br>
2023-01-03 20:19:57 -0300

    update

--- tests/gen_main.sh
+++ tests/gen_main.sh
@@ -1,4 +1,5 @@
 
 
-prun_jobs.py -c "gen_zee.py --pileupAvg 0 --nov %NOV --eventNumber %OFFSET -o %OUT -s %SEED" -nt 1 --nov 10 --novPerJob 100 -o Zee.EVT.root -m
+cd /volume/data/main/
+prun_jobs.py -c "gen_zee.py --pileupAvg 0 --nov %NOV --eventNumber %OFFSET -o %OUT -s %SEED" -nt 1 --nov 10 --novPerJob 100 -o main.EVT.root -m
 
---
+++ tests/workdir.sh
@@ -0,0 +1,9 @@
+
+
+mkdir -p volume/data/main/EVT
+mkdir -p volume/data/main/HIT
+mkdir -p volume/data/main/ESD
+mkdir -p volume/data/main/AOD
+mkdir -p volume/data/minbias/EVT
+mkdir -p volume/data/minbias/HIT 
+
