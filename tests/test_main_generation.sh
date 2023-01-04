2728549a46856bdd03755a8f699f69b04ea80814
jodafons <jodafons@lps.ufrj.br>
2023-01-03 23:28:48 -0300

    update

--- tests/test_main_generation.sh
+++ tests/test_main_generation.sh
@@ -3,8 +3,6 @@
 nov=10
 output=main.EVT.root
 
-echo "Main event generation..."
-mkdir main_generation && main_generation
 
 prun_jobs.py -c "gen_zee.py --pileupAvg 0 --nov %NOV --eventNumber %OFFSET -o %OUT -s %SEED" -nt 1 --nov $nov -o $output -m
-echo "done"
+
