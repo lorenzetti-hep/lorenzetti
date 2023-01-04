0fb574c6aea09a0cf0153c8941a9ed0d4652d87c
jodafons <jodafons@lps.ufrj.br>
2023-01-04 00:02:18 -0300

    update

--- tests/test_generation.sh
+++ tests/test_generation.sh
@@ -6,8 +6,8 @@ nov=10
 seed=512
 
 # main event generation command
-prun_jobs.py -c "gen_zee.py --pileupAvg 0 --nov %NOV --eventNumber %OFFSET -o %OUT -s %SEED" -nt 1 -s $seed --nov $nov -o Zee.EVT.root -m
+prun_jobs.py -c "gen_zee.py --pileupAvg 0 --nov %NOV --eventNumber %OFFSET -o %OUT -s %SEED" -nt 1 --seed $seed --nov $nov -o Zee.EVT.root -m
 
 # minimum bias event generation command
-prun_jobs.py -c "gen_minbias.py --pileupAvg 10 --nov %NOV --eventNumber %OFFSET -o %OUT -s %SEED" -nt 1 -s $seed --nov $nov -o mb.EVT.root -m
+prun_jobs.py -c "gen_minbias.py --pileupAvg 10 --nov %NOV --eventNumber %OFFSET -o %OUT -s %SEED" -nt 1 --seed $seed --nov $nov -o mb.EVT.root -m
 
