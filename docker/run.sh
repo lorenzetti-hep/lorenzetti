2dd18d8326a9d973f5b7d2108e378244b2c76116
jodafons <jodafons@lps.ufrj.br>
2023-05-25 20:16:43 -0400

    update

--- docker/run.sh
+++
@@ -1,2 +0,0 @@
-xhost + 
-docker run -e DISPLAY=$DISPLAY -v ${HOME}:${HOME} -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTHORITY:/tmp/.XAuthority -e XAUTHORITY=/tmp/.XAuthority  -it lorenzetti/lorenzetti:latest
