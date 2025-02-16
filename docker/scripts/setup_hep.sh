40f6ca744551552c0ac401ccb1c3d2b645f3d81b
lucasbanunes <lucasbanunes@gmail.com>
2025-02-15 22:25:59 -0300

    Adds new dockerfile without the need to setup env variables.

--- docker/scripts/setup_hep.sh
+++ docker/scripts/setup_hep.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 current_path=$PWD
 
-
-# ROOT
-echo "setup root..."
-source /hep/root/build/bin/thisroot.sh
-
 # geant4
 echo "setup geant..."
 cd /hep/geant4/build
@@ -32,10 +27,30 @@ export HEPMC_LIBRARIES=/usr/local/lib
 export LD_PRELOAD=''
 for file in /hep/geant4/build/BuildProducts/lib/*.so
 do
-  echo $file
   export LD_PRELOAD=$file:$LD_PRELOAD
 done
 
-
 cd $current_path
+
+[[ -n "$PATH" ]] && echo "PATH=$PATH"
+[[ -n "$LD_LIBRARY_PATH" ]] && echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+[[ -n "$PYTHONPATH" ]] && echo "PYTHONPATH=$PYTHONPATH"
+[[ -n "$G4NEUTRONHPDATA" ]] && echo "G4NEUTRONHPDATA=$G4NEUTRONHPDATA"
+[[ -n "$G4LEDATA" ]] && echo "G4LEDATA=$G4LEDATA"
+[[ -n "$G4LEVELGAMMADATA" ]] && echo "G4LEVELGAMMADATA=$G4LEVELGAMMADATA"
+[[ -n "$G4RADIOACTIVEDATA" ]] && echo "G4RADIOACTIVEDATA=$G4RADIOACTIVEDATA"
+[[ -n "$G4PARTICLEXSDATA" ]] && echo "G4PARTICLEXSDATA=$G4PARTICLEXSDATA"
+[[ -n "$G4PIIDATA" ]] && echo "G4PIIDATA=$G4PIIDATA"
+[[ -n "$G4REALSURFACEDATA" ]] && echo "G4REALSURFACEDATA=$G4REALSURFACEDATA"
+[[ -n "$G4SAIDXSDATA" ]] && echo "G4SAIDXSDATA=$G4SAIDXSDATA"
+[[ -n "$G4ABLADATA" ]] && echo "G4ABLADATA=$G4ABLADATA"
+[[ -n "$G4INCLDATA" ]] && echo "G4INCLDATA=$G4INCLDATA"
+[[ -n "$G4ENSDFSTATEDATA" ]] && echo "G4ENSDFSTATEDATA=$G4ENSDFSTATEDATA"
+[[ -n "$PYTHIA8_INCLUDE" ]] && echo "PYTHIA8_INCLUDE=$PYTHIA8_INCLUDE"
+[[ -n "$PYTHIA8_LIBRARIES" ]] && echo "PYTHIA8_LIBRARIES=$PYTHIA8_LIBRARIES"
+[[ -n "$FASTJET_INCLUDE" ]] && echo "FASTJET_INCLUDE=$FASTJET_INCLUDE"
+[[ -n "$FASTJET_LIBRARIES" ]] && echo "FASTJET_LIBRARIES=$FASTJET_LIBRARIES"
+[[ -n "$HEPMC_INCLUDE" ]] && echo "HEPMC_INCLUDE=$HEPMC_INCLUDE"
+[[ -n "$HEPMC_LIBRARIES" ]] && echo "HEPMC_LIBRARIES=$HEPMC_LIBRARIES"
+[[ -n "$LD_PRELOAD" ]] && echo "LD_PRELOAD=$LD_PRELOAD"
 
