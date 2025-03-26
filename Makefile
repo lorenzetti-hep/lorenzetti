e27ceb54d07fefa8b01d2f5635de96b050104851
jodafons <jodafons@lps.ufrj.br>
2025-03-26 10:31:52 -0300

    update

--- Makefile
+++ Makefile
@@ -4,28 +4,9 @@ DOCKER_NAMESPACE=lorenzetti
 
 all:  build_sources
 
-build_image:
-	docker build --progress=plain --compress -t ${DOCKER_NAMESPACE}/lorenzetti:latest .
-
-build_singularity:
-	singularity build --sandbox lorenzetti_latest.sif docker://${DOCKER_NAMESPACE}/lorenzetti:latest
-
 build_sources:
 	mkdir -p build
-	cd build && cmake .. && make -j10 && cd ..
+	cd build && cmake .. && make -j${nproc} && cp ../scripts/lzt_setup.sh . && cd ..
 
 test:
 	source .github/workflows/tests/test_local.sh tests
-
-push:
-	docker push ${DOCKER_NAMESPACE}/lorenzetti:latest
-
-run:
-	xhost + && docker run -e DISPLAY=${DISPLAY} -v ${HOME}:${HOME} -v /tmp/.X11-unix:/tmp/.X11-unix -v ${XAUTHORITY}:/tmp/.XAuthority -e XAUTHORITY=/tmp/.XAuthority  -it ${DOCKER_NAMESPACE}/lorenzetti:latest
-
-clean:
-	docker system prune -a
-
-pull:
-	singularity pull docker://lorenzetti/lorenzetti:latest
-	
---
+++ scripts/lzt_setup.sh
@@ -0,0 +1,64 @@
+
+
+CURRENT_PATH=$PWD
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $SCRIPT_DIR
+
+
+
+# Link all pcms
+rm -rf lib
+mkdir lib
+# first level pcm
+for file in "`pwd`"/*/*/*/*.pcm
+do
+  echo "ln -sf $file"
+  ln -sf $file
+done
+
+for file in "`pwd`"/*/*/*.pcm
+do
+  echo "ln -sf $file"
+  ln -sf $file
+done
+
+
+# Link all libs
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  for file in "`pwd`"/*.dylib
+  do
+    echo "ln -sf $file lib"
+    ln -sf $file lib
+  done
+else # Unix system
+  for file in "`pwd`"/*.so
+  do
+    echo "ln -sf $file lib"
+    ln -sf $file lib
+  done
+fi
+
+# link all executables from generator package
+rm -rf executables
+mkdir executables 
+for file in $PWD/*/*/*.exe
+do
+  echo "copy executable: $file"
+  ln -s $file executables
+done
+
+#
+# Add libs and python to the path
+#
+export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
+export PATH=$PWD/lib:$PATH:$PWD:$PWD/executables:$PWD/scripts/filters:$PWD/scripts/reco:$PWD/scripts/ATLAS
+export PYTHONPATH=$PWD/python:$PYTHONPATH
+export LORENZETTI_EVTGEN_DATA_DIR=$PWD/data/evtgen
+export LORENZETTI_ATLAS_DATA_DIR=$PWD/data/ATLAS
+export LC_ALL=''
+export RCM_NO_COLOR=0
+export RCM_GRID_ENV=0
+
+
+cd $CURRENT_PATH
